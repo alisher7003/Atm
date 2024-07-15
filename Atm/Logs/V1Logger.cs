12eb9364112debb34344259766ea0a264c507e88
alisher7003 <alisher.ishniyozov@gmail.com>
2024-07-15 22:04:42 +0500

    Loggers unignored

---
+++ Atm/Logs/V1Logger.cs
@@ -0,0 +1,11 @@
+﻿namespace Atm
+{
+    internal class V1Logger : ILogger
+    {
+        public void LogInformation(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Atm/Logs/V2Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atm
+{
+    internal class V2Logger : ILogger
+    {
+        public void LogInformation(string message) 
+        {            
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(message);
+        }
+    }
+}
